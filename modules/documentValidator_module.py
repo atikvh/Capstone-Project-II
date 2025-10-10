6e3adcb980775960d802dc5f110efca2f323ebcf
atikvh <atikvhhaziqvh@gmail.com>
2025-10-10 23:16:19 +0800

    starting first module, validate files uploaded

---
+++ modules/documentValidator_module.py
@@ -0,0 +1,24 @@
+"""
+This handles the validation to confirm system is able to work
+with the uploaded files. It checks the format and size of the file.
+"""
+import os
+
+class DocumentValidator:
+    SUPPORTED_FORMATS = [".pdf", ".jpg", ".jpeg", ".png"]
+    MAX_FILE_SIZE_MB = 10
+
+    def __init__(self):
+        pass
+
+    def check_format(self, file_path: str) -> bool:
+        ext = os.path.splitext(file_path)[1].lower() #1. Seperate path e.g file.PDF -> file,.pdf
+        if ext not in self.SUPPORTED_FORMATS: #2. Check format from list
+            raise ValueError(f"Unsupported file format: {ext}.\nOnly {self.SUPPORTED_FORMATS} is allowed.")
+        return True
+    
+    def check_size(self, file_path: str) -> bool:
+        file_size_MB = os.path.getsize(file_path) / (1024*1024) #1. Obtain file size
+        if file_size_MB > self.MAX_FILE_SIZE_MB: # 2. Check file size against max size
+            raise ValueError(f"File too large: {file_size_MB:.2f}MB. \nMax allowed is {self.MAX_FILE_SIZE_MB}MB.")
+        return True
